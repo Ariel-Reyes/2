a0574dfe8a85ada710fe41e651f62f47fa9b2f2f
Ariel-Reyes <73078760+Ariel-Reyes@users.noreply.github.com>
2021-02-05 13:34:20 -0600

    Agregacion del menu

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std; 
+// Ariel Fernando Reyes Castellanos (11921023)
+int main() {
+	
+	int opc =2;
+	while(opc!=5){
+		
+		cout<<"------------Menu------------"<<endl;
+		cout<<"1....................... Retorna letras unicas"<<endl; 
+		cout<<"2.........................Cuenta de cada caracter"<<endl; 
+		cout<<"3.........................De mayor a menor"<<endl;
+		cout<<"4..........................Frecuencia de letras"<<endl; 
+		cout<<"5..........................Salir"<<endl; 
+		cin>>opc; 
+		
+		
+		switch(opc){
+			case 1:
+				break; 
+				case 2:
+					break; 
+					case 3:
+						break; 
+						case 4:
+							break; 
+			
+		}
+		
+		
+		
+		
+		
+	}
+	 
+	
+	
+	
+	
+	
+	
+	return 0;
+}
